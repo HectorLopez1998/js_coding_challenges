3243442d85d81bf8eb1a2dc6356ae040c26b50b0
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-10-08 13:06:52 -0400

    initial commit

---
+++ challenge_2/script.js
@@ -0,0 +1,6 @@
+// Coding Challenge #2
+// Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+// Your tasks:
+// 1. Printaniceoutputtotheconsole,sayingwhohasthehigherBMI.Themessage is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. UseatemplateliteraltoincludetheBMIvaluesintheoutputs.Example:"Mark's BMI (28.3) is higher than John's (23.9)!"
+
