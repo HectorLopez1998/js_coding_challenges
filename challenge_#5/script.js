1dc12e990610d9c4d93bf3165f63ac6315317229
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-10-11 11:36:07 -0400

    solution

--- challenge_#5/script.js
+++ challenge_#5/script.js
@@ -7,8 +7,28 @@
 // 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
 // 2. Use the function to calculate the average for both teams
 // 3. Create a function 'checkWinner' that takes the average score of each team
-// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)"
+// as parameters ('avgDolphins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)"
 // 4. Use the 'checkWinner' function to determine the winner for both Data 1 and Data 2
 // 5. Ignore draws this time
 // Test data:
 // § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49 § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+let dolphins = calcAverage(44, 23, 71);
+let koalas = calcAverage(65, 54, 49);
+
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas){
+    console.log(`The Dolphins are the winners with a score of ${avgDolphins} vs ${avgKoalas}`);
+  } else if(avgKoalas >= 2 * avgDolphins) {
+    console.log(`The Koalas are the winners with a score of ${avgKoalas} vs ${avgDolphins};`)
+  } else {
+    console.log(`No winner this time, Dolphins with ${avgDolphins} points and Koalas with ${avgKoalas} points.`);
+  }
+  
+}
+checkWinner(dolphins, koalas);
+dolphins = calcAverage(85, 54, 41);
+koalas = calcAverage(23, 34, 27);
+checkWinner(dolphins, koalas);
